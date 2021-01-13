dd946a517994be3c554b688a181b813cdd3547c7
okamotonr <r.okamoton@gmail.com>
2021-01-13 14:47:53 +0900

    base obj

---
+++ train_aws/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def hello():
+    return 'hello world'
+
+@app.route('/home')
+def home():
+    return 'home'
+
