1af738bebce40fff9d8e058045124366a320d814
okamotonr <r.okamoton@gmail.com>
2021-01-14 15:32:21 +0900

    add

--- train_aws/app.py
+++ train_aws/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,4 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/', methods=['GET'])
+def get():
+	return render_template('index.html', \
+		title = 'Form Sample(get)', \
+		message = 'input your name')
 
+@app.route('/', methods=['POST'])
+def post():
+	name = request.form['name']
+	return render_template('index.html', \
+		title = 'Form Sample(post)', \
+		message = 'hello, {}'.format(name))
